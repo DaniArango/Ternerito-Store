6888afb66456284d14a4fcb38915af539db9f76b
DaniAr <daniarangop93@gmail.com>
2023-01-14 13:19:46 +0100

    Add design items

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { ProductsContext } from "../../context/ProductsState";
+import { Button, Card } from 'antd';
+import { BsFillCartCheckFill, BsFillCartDashFill } from "react-icons/bs";
 
 const ApiFake = () => {
   const {fakeProducts, getApiProducts } = useContext(ProductsContext);
@@ -10,18 +12,22 @@ const ApiFake = () => {
 
   return (
     <div className="container-products">
-    
   {fakeProducts.map((productfake) => {
     return (
-      <div key={productfake.id}>
-        <div>
-          <p >{productfake.title}</p>
-          <p >{productfake.category}</p>
-          <p >{productfake.price}</p>
-          <img src={productfake.image } width="100" height="100" alt=""  /> <br />
-          <p >{productfake.description}</p>
-        </div>  
-        </div>
+      <Card
+      title={productfake.title}
+      bordered={false}
+      style={{
+        width: 300,
+      }}
+    >
+    <p >{productfake.category}</p>
+    <p >{productfake.price}</p>
+    <img src={productfake.image } width="100" height="100" alt=""  /> <br />
+    <br/> 
+    <Button> <BsFillCartCheckFill /> </Button>
+    <Button> <BsFillCartDashFill /> </Button>
+    </Card>
     )}
   )};
   </div>
