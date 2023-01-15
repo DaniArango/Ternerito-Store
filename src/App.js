bb4e10583e6ea9690080373a1f0a3c88f134df61
DaniAr <daniarangop93@gmail.com>
2023-01-15 16:21:30 +0100

    fixing pagination

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Products from './components/Products/Products';
-import { ProductsProvider } from './context/ProductsState';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
+import { ProductsProvider } from "./context/ProductsState";
 
 function App() {
   return (
     <div>
       <ProductsProvider>
-       <BrowserRouter>
-      <Header />
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/products" element={<Products />} /> 
-      </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+          </Routes>
+        </BrowserRouter>
       </ProductsProvider>
     </div>
   );
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,49 +1,58 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ProductsContext } from "../../context/ProductsState";
 import { AiOutlineDoubleLeft } from "react-icons/ai";
 import { AiOutlineDoubleRight } from "react-icons/ai";
 import "./Products.scss";
 
-
-
 const ApiFake = () => {
   const { fakeProducts, getApiProducts } = useContext(ProductsContext);
+  const [page, setPage]=useState(1)
+
+
+  const limit = 8;
 
   useEffect(() => {
     getApiProducts();
   }, []);
 
   return (
-    
-       <div className="btnscontainer">
-       <button className="btnsnext"><AiOutlineDoubleLeft /> Previous</button>
-       <button className="btnsnext">Next <AiOutlineDoubleRight /> </button>
-      
+    <div className="productsview">
       <div className="container-products">
-      {fakeProducts.map((productfake) => {
-        return (
-          <div key={productfake.id} className="products">
-            <p 
-              style={{
-                width: 250,
-              }}
-            >
-            {productfake.title}
-              <hr />
-            </p>
+        {fakeProducts
+          .filter((productfake, index) => {
+            return index >= 0 + (page - 1) * limit && index < limit * page;
+          })
+          .map((productfake) => {
+            return (
+              <div key={productfake.id} className="products">
+                <br />
+                <img src={productfake.image} />
+                <br />
+                <p>
+                  <strong>{productfake.title}</strong>
+                </p>
+                <br />
+                <p>{productfake.category}</p>
+                <p>{productfake.price}€</p>
+                <button className="btn">Shop Now</button>
+                <br />
+              </div>
+            );
+          })}
+      </div>
+      <div className="btnscontainer">
+        <button className="btnsnext" onClick={()=>{
+          setPage(page -1)
+          window.scrollTo(0, 0);}}>
+          <AiOutlineDoubleLeft /> Previous
+        </button>
+        <button className="btnsnext" onClick={()=>{
+          setPage(page +1)
+          window.scrollTo(0, 0);}}>
+          Next <AiOutlineDoubleRight />{" "}
 
-            <br />
-            <img src={productfake.image} width="150" height="150" />
-            <br />
-            <br />
-            <p>Category: {productfake.category}</p>
-            <p>{productfake.price}€</p>
-            <button className="btn">Shop Now</button>
-            <br />
-          </div>
-        );
-      })}
-    </div>
+        </button>
+      </div>
     </div>
   );
 };
